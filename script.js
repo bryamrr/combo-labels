8ff83c2fe18615a9a64930715194364e2300dc0f
Bryam Rodriguez <brodriguezaybar@gmail.com>
2016-07-19 17:13:45 -0500

    Add UI and behaviour to add tags

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('#combo-labels').on('change paste keyup', function () {
+  $('#combo-labels').on('paste keyup', function () {
     var filteredDataList = [];
     if ($(this).val().length == 0) {
       $('#data-list').hide();
@@ -9,6 +9,8 @@ $(document).ready(function () {
       filterDataList(dataList, $(this).val());
     }
   });
+
+  calcInputWidth();
 });
 
 $(document).on('mouseenter', '#data-list li', function () {
@@ -20,6 +22,17 @@ $(document).on('mouseleave', '#data-list li', function () {
   $('#data-list li').removeClass('active');
 });
 
+$(document).on('click', '#data-list li', function () {
+  var id = $(this).data("id");
+  var title = $(this).text();
+
+  $('#selected-items').append('<li data-id="' + id + '">' + title + '</li>');
+  removeItemFromList(id);
+  $('#combo-labels').val('');
+
+  calcInputWidth();
+});
+
 function filterDataList(dataList, input) {
   var lonDataList = dataList.length;
   var i = 0;
@@ -27,22 +40,55 @@ function filterDataList(dataList, input) {
   $('#data-list li').remove();
 
   for (i; i < lonDataList; i++) {
-    if (dataList[i].toLowerCase().indexOf(input.toLowerCase()) != -1) {
-      $('#data-list').append('<li>' + dataList[i] + '</li>');
+    if (dataList[i].title.toLowerCase().indexOf(input.toLowerCase()) != -1) {
+      $('#data-list').append('<li data-id="' + dataList[i].id+ '">' + dataList[i].title + '</li>');
     }
   }
 
   $($('#data-list li')[0]).addClass('active');
 }
 
+function removeItemFromList(id) {
+  var lonDataList = dataList.length;
+  var i = 0;
+
+  for (i; i < lonDataList; i++) {
+    if (dataList[i].id === parseInt(id)) {
+      $('#data-list li[data-id="' + id + '"]').remove();
+      dataList.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function calcInputWidth() {
+
+  var containerWidth = $('.input-container').width();
+  var itemsWidth = $('#selected-items').width();
+
+  $('.input-container input').width(containerWidth - 6 - itemsWidth);
+}
+
 var dataList = [
-  'Andres',
-  'Rafael',
-  'Hugo',
-  'Katherine',
-  'Percy',
-  'Claudio',
-  'Oscar',
-  'Bryam',
-  'John'
+  { id: 1, title: 'Andres' },
+  { id: 2, title: 'Rafael' },
+  { id: 3, title: 'Hugo' },
+  { id: 4, title: 'Katherine' },
+  { id: 5, title: 'Percy' },
+  { id: 6, title: 'Claudio' },
+  { id: 7, title: 'Oscar' },
+  { id: 8, title: 'Bryam' },
+  { id: 9, title: 'John' }
+];
+
+var originalList = [
+  { id: 1, title: 'Andres' },
+  { id: 2, title: 'Rafael' },
+  { id: 3, title: 'Hugo' },
+  { id: 4, title: 'Katherine' },
+  { id: 5, title: 'Percy' },
+  { id: 6, title: 'Claudio' },
+  { id: 7, title: 'Oscar' },
+  { id: 8, title: 'Bryam' },
+  { id: 9, title: 'John' }
 ];
